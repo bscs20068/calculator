36857347d8a49641a29b6507f1fc8716bbea34ee
bscs20068 <107097100+bscs20068@users.noreply.github.com>
2023-10-14 19:05:57 +0500

    Update build.gradle.kts
    the 1 change by usman

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,8 @@
+buildscript {
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.0.1")
+    }
+}
 plugins {
     alias(libs.plugins.android).apply(false)
     alias(libs.plugins.kotlinAndroid).apply(false)
